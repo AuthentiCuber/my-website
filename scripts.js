21fb6fae15de6f20defa920f3364312617358075
Harrison O'Reibow <harrison.oreibow@gmail.com>
2023-07-29 22:22:12 +1200

    localstorage

--- scripts.js
+++ scripts.js
@@ -1,16 +1,31 @@
 const icon = document.getElementById("theme-button");
 const body = document.body;
 
-const toggleTheme = () => {
-   body.classList.toggle("dark");
-   icon.classList.toggle("fa-moon");
-   icon.classList.toggle("fa-sun");
+const setTheme = (theme) => {
+   body.className = theme;
+   if (theme == "light") {
+      icon.classList.remove("fa-moon");
+      icon.classList.add("fa-sun");
+   } else {
+      icon.classList.remove("fa-moon");
+      icon.classList.add("fa-sun");
+   }
+   localStorage.theme = body.className;
 };
 
-// https://stackoverflow.com/questions/56393880/how-do-i-detect-dark-mode-using-javascript
-if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-   toggleTheme();
+const toggleTheme = () => {
+   setTheme(localStorage.theme == "light" ? "dark" : "light")
 }
+
+if (localStorage.theme) {
+   setTheme(localStorage.theme);
+   // https://stackoverflow.com/questions/56393880/how-do-i-detect-dark-mode-using-javascript
+} else if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+   setTheme("dark");
+} else {
+   setTheme("light");
+}
+
 
 const tooltips = document.querySelectorAll("[data-tool-tip]");
 
